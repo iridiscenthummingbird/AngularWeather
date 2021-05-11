7414dd28cf92605dcb67b9ea0b4794cb236e60d0
iridiscent-hummingbird <0508dima@gmail.com>
2021-05-11 17:09:17 +0200

    Init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NowComponent } from './now/now.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NowComponent
   ],
   imports: [
     BrowserModule
